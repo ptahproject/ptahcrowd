e0637b4afb5502d8156f3b8218d5f5a6408b3bd2
Nikolay Kim <fafhrd91@gmail.com>
2011-11-29 01:56:36 -0600

    add CrowdUser to public api

--- ptah_crowd/__init__.py
+++ ptah_crowd/__init__.py
@@ -1,5 +1,7 @@
 # ptah_crowd package
 
+from ptah_crowd.provider import CrowdUser, Session
+
 from ptah_crowd.settings import CROWD as CONFIG
 from ptah_crowd.validation import initiate_email_validation
 from ptah_crowd.memberprops import get_properties
