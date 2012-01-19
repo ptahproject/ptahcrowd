db9978239c7d321c179121b92d4772009b95a571
Nikolay Kim <fafhrd91@gmail.com>
2012-01-19 11:08:56 -0600

    ptah dep

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 version='0.2dev'
 
 install_requires = ['setuptools',
-                    'ptah >= 0.3.0',
+                    'ptah >= 0.3.1',
                     "oauth2 >= 1.1.3",
                     "requests >= 0.6.6",
                     ]
