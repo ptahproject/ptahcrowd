8728fdd882fc321e17587078c191f15e9253ecf9
Nikolay Kim <fafhrd91@gmail.com>
2012-02-09 12:28:43 -0600

    remove change password form in user management

--- ptah_crowd/user.py
+++ ptah_crowd/user.py
@@ -134,8 +134,8 @@ class ModifyUserForm(form.Form):
                 'password': '',
                 'validated': props.validated,
                 'suspended': props.suspended,
-                'roles': props.data.get('roles'),
-                'groups': props.data.get('groups')}
+                'roles': props.data.get('roles', ()),
+                'groups': props.data.get('groups', ())}
 
     @form.button(_('Modify'), actype=form.AC_PRIMARY)
     def modify(self):
@@ -177,42 +177,6 @@ class ModifyUserForm(form.Form):
         self.message("User has been removed.", 'info')
         return HTTPFound(location='..')
 
-    @form.button(_('Change password'), name='changepwd')
-    def password(self):
-        return HTTPFound(location='password.html')
-
     @form.button(_('Back'))
     def back(self):
         return HTTPFound(location='..')
-
-
-@view_config('password.html',
-             context=CrowdUser,
-             wrapper=ptah.wrap_layout(),
-             route_name=ptah_crowd.CROWD_APP_ID)
-
-class ChangePasswordForm(form.Form):
-
-    csrf = True
-    fields = form.Fieldset(ManagerChangePasswordSchema)
-
-    label = _('Change password')
-    description = _('Please specify password for this users.')
-
-    @form.button(_('Back'))
-    def back(self):
-        return HTTPFound(location='..')
-
-    @form.button(_('Change'), actype=form.AC_PRIMARY)
-    def change(self):
-        data, errors = self.extract()
-
-        if errors:
-            self.message(errors, 'form-error')
-            return
-
-        sm = self.request.registry
-
-        self.context.password = ptah.pwd_tool.encode(data['password'])
-
-        self.message("User password has been changed.")
